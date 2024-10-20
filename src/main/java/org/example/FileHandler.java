673aba639cd2171c4fc6ca25642596ebf7943bf0
Fejkel <KacperMik529@gmail.com>
2024-10-20 03:38:32 +0200

    FileHandler, guest object.

---
+++ src/main/java/org/example/FileHandler.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileHandler {
+    private final String absPath;
+
+    public FileHandler(String absPath){
+        this.absPath = absPath;
+        this.validFile();
+
+    }
+    public void validFile()
+    {
+        File inputFile = new File(absPath);
+        try {
+            Scanner fileScanner = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot open input file at path " + this.absPath);
+        }
+    }
+
+    public ArrayList<Guest> getGuestsList() throws FileNotFoundException {
+        String readline;
+        File inputFile = new File(this.absPath);
+        Scanner fileScanner = new Scanner(inputFile);
+        ArrayList<Guest> GuestsList = new ArrayList<>();
+
+        while (fileScanner.hasNextLine())
+        {
+            ArrayList<String> possessed = new ArrayList<>();
+            ArrayList<String> sought = new ArrayList<>();
+
+            readline = fileScanner.nextLine();
+
+            String temp = readline.split("\t")[1];
+            possessed.add(Arrays.toString(temp.split(",")));
+            temp = readline.split("\t")[2];
+            sought.add(Arrays.toString(temp.split(",")));
+
+            Guest guest = new Guest();
+            guest.setPossessed_attributes(possessed);
+            guest.setSought_attributes(sought);
+            GuestsList.add(guest);
+        }
+        return GuestsList;
+    }
+}
---
+++ src/main/java/org/example/Guest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Guest {
+    private ArrayList<String> possessed_attributes;
+    private ArrayList<String> sought_attributes;
+
+    public Guest() {}
+
+
+    //settery
+    public void setPossessed_attributes(ArrayList<String> possessed_attributes) {
+        this.possessed_attributes = possessed_attributes;
+    }
+    public void setSought_attributes(ArrayList<String> sought_attributes) {
+        this.sought_attributes = sought_attributes;
+    }
+    //gettery
+    public ArrayList<String> getPossessed_attributes() {
+        return possessed_attributes;
+    }
+    public ArrayList<String> getSought_attributes() {
+        return sought_attributes;
+    }
+}
